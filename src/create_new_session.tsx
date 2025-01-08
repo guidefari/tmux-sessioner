f0364aaadb7d902ef84d62bf7607071036ac1819
Guide Fari <guideg6@gmail.com>
2025-01-08 08:42:17 +0200

    feat: specify directory

--- src/create_new_session.tsx
+++ src/create_new_session.tsx
@@ -1,73 +1,125 @@
 import { useState } from "react";
-import { Action, ActionPanel, Form, popToRoot, showToast, Toast } from "@raycast/api";
-import { createNewSession, getAllSession } from "./utils/sessionUtils";
+import {
+	Action,
+	ActionPanel,
+	Form,
+	getPreferenceValues,
+	popToRoot,
+	showToast,
+	Toast,
+} from "@raycast/api";
+import {
+	createNewSession,
+	directoryExists,
+	getAllSession,
+} from "./utils/sessionUtils";
 
 export default function CreateNewTmuxSession() {
-  const [loading, setLoading] = useState<boolean>(false);
-  const [sessionNameError, setSessionNameError] = useState<string>("");
+	const [loading, setLoading] = useState<boolean>(false);
+	const [sessionNameError, setSessionNameError] = useState<string>("");
+	const [sessionDirectoryError, setSessionDirectoryError] =
+		useState<string>("");
+	const preferences = getPreferenceValues();
 
-  return (
-    <Form
-      isLoading={loading}
-      navigationTitle="Create New Tmux Session"
-      actions={
-        <ActionPanel>
-          <Action.SubmitForm
-            title="Create New Session"
-            onSubmit={async (values) => {
-              const sessionName = values.newSessionName;
-              setLoading(true);
+	return (
+		<Form
+			isLoading={loading}
+			navigationTitle="Create New Tmux Session"
+			actions={
+				<ActionPanel>
+					<Action.SubmitForm
+						title="Create New Session"
+						onSubmit={async (values) => {
+							const sessionName = values.newSessionName;
+							const sessionDirectory = values.newSessionDirectory[0];
+							setLoading(true);
 
-              const toast = await showToast({
-                style: Toast.Style.Animated,
-                title: "",
-              });
+							const toast = await showToast({
+								style: Toast.Style.Animated,
+								title: "",
+							});
 
-              createNewSession(sessionName, (error, _stdout, stderr) => {
-                if (error || stderr) {
-                  console.error(`exec error: ${error}`);
-                  setLoading(false);
-                  toast.style = Toast.Style.Failure;
-                  toast.message = "Failed to create new session";
-                  return;
-                }
+							if (!sessionName) {
+								const errorMessage = "Session name is required";
+								setSessionNameError(errorMessage);
+								toast.style = Toast.Style.Failure;
+								toast.message = errorMessage;
+								setLoading(false);
+								return;
+							}
 
-                toast.style = Toast.Style.Success;
-                toast.style = Toast.Style.Success;
-                toast.message = `New session ${sessionName} is setup successfully`;
-                setLoading(false);
-                popToRoot();
-              });
-            }}
-          />
-        </ActionPanel>
-      }
-    >
-      <Form.TextField
-        title="New Session Name"
-        id="newSessionName"
-        error={sessionNameError}
-        onChange={(value) => {
-          if (!value || value.length === 0) {
-            return;
-          }
+							if (sessionDirectory && !directoryExists(sessionDirectory)) {
+								const errorMessage =
+									"The directory you selected does not exist";
+								setSessionDirectoryError(errorMessage);
+								toast.style = Toast.Style.Failure;
+								toast.message = errorMessage;
+								setLoading(false);
+								return;
+							}
 
-          getAllSession((error, stdout, stderr) => {
-            if (error || stderr) {
-              console.error(`exec error: ${error}`);
-              setLoading(false);
-            }
+							createNewSession(
+								sessionName,
+								sessionDirectory || preferences.defaultDirectory || "/",
+								(error, _stdout, stderr) => {
+									if (error || stderr) {
+										console.error(`exec error: ${error}`);
+										setLoading(false);
+										toast.style = Toast.Style.Failure;
+										toast.message = "Failed to create new session";
+										return;
+									}
 
-            const lines = stdout.trim().split("\n");
+									toast.style = Toast.Style.Success;
+									toast.message = `New session ${sessionName} is setup successfully`;
+									setLoading(false);
+									popToRoot();
+								},
+							);
+						}}
+					/>
+				</ActionPanel>
+			}
+		>
+			<Form.TextField
+				title="New Session Name"
+				id="newSessionName"
+				error={sessionNameError}
+				onChange={(value) => {
+					if (!value || value.length === 0) {
+						return;
+					}
 
-            if (lines.includes(value)) {
-              setSessionNameError("Session name already exists");
-            } else {
-              setSessionNameError("");
-            }
-          });
-        }}
-      />
-    </Form>
-  );
+					getAllSession((error, stdout, stderr) => {
+						if (error || stderr) {
+							console.error(`exec error: ${error}`);
+							setLoading(false);
+						}
+
+						const lines = stdout.trim().split("\n");
+
+						if (lines.includes(value)) {
+							setSessionNameError("Session name already exists");
+						} else {
+							setSessionNameError("");
+						}
+					});
+				}}
+			/>
+			<Form.FilePicker
+				title="New Session Directory"
+				id="newSessionDirectory"
+				allowMultipleSelection={false}
+				canChooseDirectories
+				canChooseFiles={false}
+				error={sessionDirectoryError}
+				onChange={(value) => {
+					if (!value || value.length === 0) {
+						return;
+					}
+					setSessionDirectoryError("");
+				}}
+			/>
+		</Form>
+	);
 }
--- src/utils/sessionUtils.ts
+++ src/utils/sessionUtils.ts
@@ -1,79 +1,119 @@
-import { ChildProcess, exec, ExecException } from "child_process";
+import {
+	type ChildProcess,
+	exec,
+	type ExecException,
+} from "node:child_process";
 import { env } from "../config";
 import { showHUD, showToast, Toast } from "@raycast/api";
 import { openTerminal } from "./terminalUtils";
-
+import fs from "node:fs";
 export function getAllSession(
-  callback: (error: ExecException | null, stdout: string, stderr: string) => void
+	callback: (
+		error: ExecException | null,
+		stdout: string,
+		stderr: string,
+	) => void,
 ): ChildProcess {
-  return exec(`tmux list-sessions | awk '{print $1}' | sed 's/://'`, { env }, callback);
+	return exec(
+		`tmux list-sessions | awk '{print $1}' | sed 's/://'`,
+		{ env },
+		callback,
+	);
+}
+
+export function directoryExists(directory: string): boolean {
+	return fs.existsSync(directory);
 }
 
 export function createNewSession(
-  sessionName: string,
-  callback: (error: ExecException | null, stdout: string, stderr: string) => void
+	sessionName: string,
+	sessionDirectory: string,
+	callback: (
+		error: ExecException | null,
+		stdout: string,
+		stderr: string,
+	) => void,
 ): ChildProcess {
-  return exec(`tmux new-session -d -s ${sessionName}`, { env }, callback);
+	return exec(
+		`tmux new-session -d -s ${sessionName} -c ${sessionDirectory}`,
+		{ env },
+		callback,
+	);
 }
 
 export function renameSession(
-  oldSessionName: string,
-  newSessionName: string,
-  callback: (error: ExecException | null, stdout: string, stderr: string) => void
+	oldSessionName: string,
+	newSessionName: string,
+	callback: (
+		error: ExecException | null,
+		stdout: string,
+		stderr: string,
+	) => void,
 ): ChildProcess {
-  return exec(`tmux rename-session -t ${oldSessionName} ${newSessionName}`, { env }, callback);
+	return exec(
+		`tmux rename-session -t ${oldSessionName} ${newSessionName}`,
+		{ env },
+		callback,
+	);
 }
 
-export async function switchToSession(session: string, setLoading: (value: boolean) => void) {
-  const toast = await showToast({ style: Toast.Style.Animated, title: "" });
-  setLoading(true);
+export async function switchToSession(
+	session: string,
+	setLoading: (value: boolean) => void,
+) {
+	const toast = await showToast({ style: Toast.Style.Animated, title: "" });
+	setLoading(true);
 
-  exec(`tmux switch -t ${session}`, { env }, async (error, stdout, stderr) => {
-    if (error || stderr) {
-      console.error(`exec error: ${error || stderr}`);
+	exec(`tmux switch -t ${session}`, { env }, async (error, stdout, stderr) => {
+		if (error || stderr) {
+			console.error(`exec error: ${error || stderr}`);
 
-      toast.style = Toast.Style.Failure;
-      toast.title = "No tmux client found 😢";
-      toast.message = error ? error.message : stderr;
-      setLoading(false);
+			toast.style = Toast.Style.Failure;
+			toast.title = "No tmux client found 😢";
+			toast.message = error ? error.message : stderr;
+			setLoading(false);
 
-      return;
-    }
+			return;
+		}
 
-    try {
-      await openTerminal();
+		try {
+			await openTerminal();
 
-      toast.style = Toast.Style.Success;
-      toast.title = `Switched to session ${session}`;
-      await showHUD(`Switched to session ${session}`);
-      setLoading(false);
-    } catch (e) {
-      toast.style = Toast.Style.Failure;
-      toast.title = "Terminal not supported 😢";
-      setLoading(false);
-    }
-    return;
-  });
+			toast.style = Toast.Style.Success;
+			toast.title = `Switched to session ${session}`;
+			await showHUD(`Switched to session ${session}`);
+			setLoading(false);
+		} catch (e) {
+			toast.style = Toast.Style.Failure;
+			toast.title = "Terminal not supported 😢";
+			setLoading(false);
+		}
+		return;
+	});
 }
 
-export async function deleteSession(session: string, setLoading: (value: boolean) => void, callback: () => void) {
-  setLoading(true);
-  const toast = await showToast({ style: Toast.Style.Animated, title: "" });
+export async function deleteSession(
+	session: string,
+	setLoading: (value: boolean) => void,
+	callback: () => void,
+) {
+	setLoading(true);
+	const toast = await showToast({ style: Toast.Style.Animated, title: "" });
 
-  exec(`tmux kill-session -t ${session}`, { env }, (error, stdout, stderr) => {
-    if (error || stderr) {
-      console.error(`exec error: ${error || stderr}`);
+	exec(`tmux kill-session -t ${session}`, { env }, (error, stdout, stderr) => {
+		if (error || stderr) {
+			console.error(`exec error: ${error || stderr}`);
 
-      toast.style = Toast.Style.Failure;
-      toast.title = "Something went wrong 😢";
-      toast.message = error ? error.message : stderr;
-      setLoading(false);
-      return;
-    }
+			toast.style = Toast.Style.Failure;
+			toast.title = "Something went wrong 😢";
+			toast.message = error ? error.message : stderr;
+			setLoading(false);
+			return;
+		}
 
-    toast.style = Toast.Style.Success;
-    toast.title = `Deleted session ${session}`;
-    callback();
-    setLoading(false);
-  });
+		toast.style = Toast.Style.Success;
+		toast.title = `Deleted session ${session}`;
+		callback();
+		setLoading(false);
+	});
 }
